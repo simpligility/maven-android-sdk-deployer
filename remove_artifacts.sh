7f5df0f58bd186aa32f9100e26c13efcbfe964f0
kewang <cpckewang@gmail.com>
2014-02-19 00:33:50 +0800

    add Darwin & Linux support script

--- remove_artifacts.sh
+++ remove_artifacts.sh
@@ -1,4 +1,19 @@
-rm -rfv ~/.m2/repository/android
-rm -rfv ~/.m2/repository/com/google/android/
-rm -rfv ~/.m2/repository/com/android/future/
+#!/bin/bash
 
+function remove_osx(){
+  rm -rfv ~/.m2/repository/android
+  rm -rfv ~/.m2/repository/com/google/android/
+  rm -rfv ~/.m2/repository/com/android/future/
+}
+
+function remove_linux(){
+  rm -rfv ~/.m2/repository/android
+  rm -rfv ~/.m2/repository/com/google/android/
+  rm -rfv ~/.m2/repository/com/android/future/
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+  remove_osx
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  remove_linux
+fi
